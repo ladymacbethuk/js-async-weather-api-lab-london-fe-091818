b0b8e40cf2c7ea6d782927a3b91feffec39ff226
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-22 08:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "114689e6ae9f35cb42a661727b783a3d"
+
 
 function handleFormSubmit(event) {
   //handle submit event
-}
+  let x = document.getElementById("city").value;
+  x.innerHTML;
+  console.log(x);
+   }
+
+
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
+  document.getElementById("submit").addEventListener("click", )
+  
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=london&APPID=114689e6ae9f35cb42a661727b783a3d');
+  // .then(response => response.json());
+  // .then(json => console.log(json));
+  //displayCurrentWeather();
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+  document.getElementById("temp").innerHTML = '';
+  document.getElementById("low").innerHTML = '';
+  document.getElementById("high").innerHTML = '';
+  document.getElementById("humidity").innerHTML = '';
+  document.getElementById("cloudCover").innerHTML = '';
+  document.getElementById("sunrise").innerHTML = '';
+  document.getElementById("sunset").innerHTML = '';
 }
 
 
